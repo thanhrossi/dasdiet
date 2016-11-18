f29987bd17f8a234efe9c97c5b34415aa8ab405b
thanhtv <thanhrossi.tt@gmail.com>
2016-11-18 16:42:29 +0700

    update init

---
+++ www/js/app.js
@@ -0,0 +1,180 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html'
+        }
+      }
+    })
+  // config chap
+  .state('app.chuong1', {
+      url: '/chuong1',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong1.html'
+        }
+      }
+    })
+  .state('app.chuong2', {
+      url: '/chuong2',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong2.html'
+        }
+      }
+    })
+  .state('app.chuong3', {
+      url: '/chuong3',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong3.html'
+        }
+      }
+    })
+    .state('app.chuong4', {
+      url: '/chuong4',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong4.html'
+        }
+      }
+    })
+      .state('app.chuong5', {
+      url: '/chuong5',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong5.html'
+        }
+      }
+    })
+  .state('app.chuong6', {
+      url: '/chuong6',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong6.html'
+        }
+      }
+    })
+  .state('app.chuong7', {
+      url: '/chuong7',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong7.html'
+        }
+      }
+    })
+    .state('app.chuong8', {
+      url: '/chuong8',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong8.html'
+        }
+      }
+    })
+      .state('app.chuong9', {
+      url: '/chuong9',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong9.html'
+        }
+      }
+    })
+  .state('app.chuong10', {
+      url: '/chuong10',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong10.html'
+        }
+      }
+    })
+  .state('app.chuong11', {
+      url: '/chuong11',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong11.html'
+        }
+      }
+    })
+    .state('app.chuong12', {
+      url: '/chuong12',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong12.html'
+        }
+      }
+    })
+      .state('app.chuong13', {
+      url: '/chuong13',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/chaps/chuong13.html'
+        }
+      }
+    })
+  // End config chap
+  .state('app.chaplists', {
+    url: '/chaplists',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/chaplists.html',
+        controller: 'ChapListsCtrl'
+      }
+    }
+  })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/about');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,63 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+
+.controller('ChapListsCtrl', function ($scope) {
+  $scope.chapLists = [
+    { title: '1- Nguyên lí căn bản của DAS Diet', id: 1},
+    { title: '2- Qui tắc Đèn Giao Thông', id: 2},
+    { title: '3- Relax - duy trì vóc dáng', id: 3},
+    { title: '4- Lowcarb diet và lợi ích đối với sức khỏe', id: 4},
+    { title: '5- Giảm mỡ và giảm cân', id: 5},
+    { title: '6- Kiểm tra quá trình ketosis bằng que thử', id: 6},
+    { title: '7- Áp dụng DAS vào cuộc sống hàng ngày, biến thành Life Style', id: 7},
+    { title: '8- Hướng dẫn sử dụng đậu phụ và các chế phẩm của đậu nành trong quá trình DAS', id: 8},
+    { title: '9- Các loại đuờng ăn kiêng', id: 9},
+    { title: '10- Giới thiệu về DAS Diet', id: 10},
+    { title: 'Phần I: Lịch sử phát triển của chế độ ăn ít chất tinh bột, đƣờng ( Low Carb )', id: 11},
+    { title: 'Phần II: Sự phát triển của chế độ dinh dƣỡng Low Carb ở Việt Nam', id: 12},
+    { title: 'Phần III: Giới thiệu về Atkins’ Diet và DAS Diet', id: 13},
+  ];
+
+
+})
