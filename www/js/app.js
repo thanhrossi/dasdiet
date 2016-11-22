904315ee129bd1882cd16e0b606083fff585c19d
thanhtv <thanhrossi.tt@gmail.com>
2016-11-22 15:15:55 +0700

    update img

--- www/js/app.js
+++ www/js/app.js
@@ -155,15 +155,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+
 
   .state('app.single', {
     url: '/playlists/:playlistId',
